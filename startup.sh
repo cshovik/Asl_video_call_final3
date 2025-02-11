6f574874ba73a7c37f362e9c0c494de0aaf36c13
SHOVIK CHAKRABORTY <113230439+cshovik@users.noreply.github.com>
2025-02-11 22:23:34 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Update package list and install required system dependencies
-apt-get update && apt-get install -y --no-install-recommends libgl1-mesa-glx
+apt-get update && apt-get install -y --no-install-recommends \
+    libgl1-mesa-glx \
+    libglib2.0-0
 
 # Start the Gunicorn server
 gunicorn app:app
+
